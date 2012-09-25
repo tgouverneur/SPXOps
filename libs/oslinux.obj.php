7a9708df389ce2566e63615bb34f38ca53236812
Thomas Gouverneur <thomas@espix.net>
2012-09-25 15:14:35 +0200

    Added package inventory for RHEL

--- libs/oslinux.obj.php
+++ libs/oslinux.obj.php
@@ -218,7 +218,33 @@ class OSLinux extends OSType
   }
 
   public static function update_packages_rpm(&$s) {
-    return array();
+    $rpm = $s->findBin('rpm');
+    $cmd_rpm = "$rpm -qa --qf '%{NAME};%{VERSION};%{ARCH};;%{SUMMARY}\n'";
+    $out_rpm = $s->exec($cmd_rpm);
+
+    $lines = explode(PHP_EOL, $out_rpm);
+    $found_p = array();
+
+    $pkg = null;
+    foreach($lines as $line) {
+      $line = trim($line);
+      if (empty($line)) {
+        continue;
+      }
+      $f = explode(';', $line);
+      if (preg_match('/not-installed/', $f[3])) {
+        continue;
+      }
+      $pkg = array();
+      $pkg['name'] = $f[0];
+      $pkg['version'] = $f[1];
+      $pkg['arch'] = $f[2];
+      $pkg['status'] = $f[3];
+      $pkg['desc'] = $f[4];
+      $found_p[$f[0]] = $pkg;
+    }
+
+    return $found_p;
   }
 
   public static function update_packages_ebd(&$s) {
