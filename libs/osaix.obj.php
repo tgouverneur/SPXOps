f2a8154e45540bf244d3517544782307683d5f30
Thomas Gouverneur <thomas@espix.net>
2012-09-25 15:02:50 +0200

    Added update_network

--- libs/osaix.obj.php
+++ libs/osaix.obj.php
@@ -5,6 +5,7 @@ class OSAix extends OSType
   public static $binPaths = array(
     "/usr/bin",
     "/usr/sbin",
+    "/etc",
     "/bin",
     "/usr/local/bin",
     "/usr/local/sbin",
@@ -16,12 +17,216 @@ class OSAix extends OSType
     "update_hostid",
     "update_oslevel",
     "update_uname",
-  //  "update_network",
+    "update_network",
   //  "update_nfs_shares",
   //  "update_nfs_mount",
   );
 
   /* Updates function for AIX */
+
+
+
+  public static function update_network_ifs(&$s) {
+
+    $found_if = array();
+
+    $sudo = $s->findBin('sudo');
+    $ifconfig = $s->findBin('ifconfig');
+    $cmd_ifconfig = "$sudo $ifconfig -a";
+    $out_ifconfig = $s->exec($cmd_ifconfig);
+
+    $lines = explode(PHP_EOL, $out_ifconfig);
+
+    $ifname = $c_if = $c_vif = $alias = null;
+    foreach($lines as $line) {
+      $line = trim($line);
+      if (empty($line))
+        continue;
+
+      $f = preg_split("/\s+/", $line);
+
+      if (preg_match('/^([a-z0-9:]*):$/', $f[0], $m)) {
+
+        $ifname = $m[1];
+        $alias = '';
+        $flags = '';
+
+        if (preg_match('/:/', $m[1])) {
+          $ifname = explode(':', $m[1]);
+          $alias = $ifname[1];
+          $ifname = $ifname[0];
+        }
+        if (preg_match('/flags=([0-9]*)<([A-Za-z0-9,]*)>/', $f[1], $m)) {
+          $flags = $m[2];
+        }
+        if (empty($alias)) {
+          // physical should match $ifname already
+          if (isset($found_if[$ifname])) {
+            if (!isset($found_if[$ifname]['flags']) ||
+                empty($found_if[$ifname]['flags'])) {
+              $found_if[$ifname]['flags'] = $flags;
+            }
+          } else {
+            $if = array();
+            $if['ifname'] = $ifname;
+            $if['flags'] = $flags;
+            $if['layer'] = 2;
+            $if['fk_server'] = $s->id;
+            /* Address hereunder... */
+            $if['addr'] = array();
+            $if['caddr'] = 0;
+            $found_if[$ifname] = $if;
+          }
+          $c_if = &$found_if[$ifname];
+        }
+
+      } else if (!strcmp($f[0], 'ether')) {
+        if (isset($found_if[$ifname])) {
+          $found_if[$ifname]['address'] = $f[1];
+        }
+      } else if (!strcmp($f[0], 'inet') && strcmp($f[1], '0.0.0.0') && $f[1] != 0) {
+        $vif = array();
+        $vif['ifname'] = $ifname;
+        $vif['alias'] = $alias;
+        $vif['layer'] = 3;
+        $vif['fk_server'] = $s->id;
+        $vif['version'] = 4;
+        $vif['address'] = $f[1];
+        if (!strcmp($f[2], 'netmask')) {
+          $vif['netmask'] = $f[3];
+        }
+        if (isset($found_if[$ifname])) {
+          $c_vif = $found_if[$ifname]['caddr'];
+          $found_if[$ifname]['addr'][$c_vif] = $vif;
+          $found_if[$ifname]['caddr']++;
+        }
+
+      } else if (!strcmp($f[0], 'inet6') && strcmp($f[1], '::/0')) {
+
+        $vif = array();
+        $vif['ifname'] = $ifname;
+        $vif['alias'] = $alias;
+        $vif['layer'] = 3;
+        $vif['version'] = 6;
+        $vif['address'] = $f[1];
+        $vif['fk_server'] = $s->id;
+        if (preg_match('/\//', $vif['address'])) {
+          $vif['address'] = explode('/', $vif['address']);
+          $vif['netmask'] = $vif['address'][1];
+          $vif['address'] = $vif['address'][0];
+        }
+        if (count($f) > 3 && !strcmp($f[2], 'netmask')) {
+          $vif['netmask'] = $f[3];
+        }
+        if (isset($found_if[$ifname])) {
+          $c_vif = $found_if[$ifname]['caddr'];
+          $found_if[$ifname]['addr'][$c_vif] = $vif;
+          $found_if[$ifname]['caddr']++;
+        }
+
+      } else if (!strcmp($f[0], 'groupname')) {
+        if ($found_if[$ifname]) {
+          $found_if[$ifname]['group'] = $f[1];
+          $found_if[$ifname]['f_ipmp'] = 1;
+        }
+      }
+    }
+    return $found_if;
+  }
+
+
+  public static function update_network(&$s) {
+
+    $ifs = OSAix::update_network_ifs($s);
+
+    $f = array(
+                'ifname',
+                'fk_server',
+                'alias',
+                'layer',
+                'version',
+                'address',
+         );
+    $fa = array(
+                'netmask',
+                'group',
+                'flags',
+                'f_ipmp',
+        );
+
+    $bifs = array();
+    foreach($ifs as $if) {
+      $bifs[] = $if;
+      $bifs = array_merge($bifs, $if['addr']);
+    }
+    $found_if = array();
+    foreach($bifs as $if) {
+      $io = new Net();
+      $upd = false;
+      foreach($f as $fi) {   
+        if (isset($if[$fi])) {
+          $io->{$fi} = $if[$fi];
+        }
+      }
+      if ($io->fetchFromFields($f)) {
+        $io->insert();
+        $s->log("Added $io", LLOG_INFO);
+      }
+      foreach($fa as $fi) {
+        if (isset($if[$fi])) {
+          if (strcmp($io->{$fi}, $if[$fi])) {
+            $io->{$fi} = $if[$fi];
+            $upd = true;
+            $s->log("Changed $io $fi to be ".$if[$fi], LLOG_DEBUG);
+          }
+        }
+      }
+      $io->fetchAll();
+      $found_if[''.$io] = $io;
+      if ($upd) {
+        $io->update();
+      }
+    }
+
+    foreach($s->a_net as $n) {
+      $n->fetchAll();
+      if (isset($found_if[''.$n])) {
+        continue;
+      }
+      $s->log("Removing net $n", LLOG_INFO);
+      $n->delete();
+    }
+
+    /* default router */
+
+    $netstat = $s->findBin('netstat');
+    $cmd_netstat = "$netstat -rn";
+    $out_netstat = $s->exec($cmd_netstat);
+
+    $lines = explode(PHP_EOL, $out_netstat);
+    $defrouter = null;
+
+    foreach($lines as $line) {
+      $line = trim($line);
+      if (empty($line))
+        continue;
+
+      $f = preg_split("/\s+/", $line);
+
+      if (!strcmp($f[0], 'default')) {
+        $defrouter = $f[1];
+        break;
+      }
+    }
+
+    if ($defrouter &&
+        strcmp($s->data('net:defrouter'), $defrouter)) {
+      $s->setData('net:defrouter', $defrouter);
+      $s->log("Change defrouter => $defrouter", LLOG_INFO);
+    }
+
+    return 0;
+  }
   
   /**
    * hostid
